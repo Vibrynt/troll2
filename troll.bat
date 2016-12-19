c1cc32c5fc6a7a32d4a9279bf91857ccaedfcd26
Vibrynt <jacksondnm84@gmail.com>
2016-12-18 16:11:44 -0800

    Add files via upload

---
+++ troll.bat
@@ -0,0 +1,5 @@
+@echo off
+msg * Warning your computer has detected a virus
+msg * To remove virus click OK or close this box
+msg * Your Hard Drives are now being formatted
+msg * Please wait.........
